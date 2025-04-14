22426ec02fa38b79c558c26458698028ffcec35c
PattyPrograms <patricktilotta@gmail.com>
2025-04-14 10:32:44 -0500

    Updated route-records.h

--- route-records.h
+++ route-records.h
@@ -1,9 +1,31 @@
-#ifndef ROUTE-RECORDS_H
-#define ROUTE-RECORDS_H
+#ifndef ROUTE_RECORDS_H
+#define ROUTE_RECORDS_H
 
+#define MAX_SIZE 20
+#include <stdio.h>
 
+typedef struct ROUTERECORD {
+    char origin[MAX_SIZE];
+    char destination[MAX_SIZE];
+    char airline[MAX_SIZE];
+    int passengerCount[6];
+} RouteRecord;
 
+typedef enum SearchType {
+    ROUTE,
+    ORIGIN,
+    DESTINATION,
+    AIRLINE
+} SearchType;
 
+RouteRecord* createRecords(FILE* fileIn);
 
+int fillRecords(RouteRecord* r, FILE* fileIn);
+
+int findAirlineRoute(RouteRecord* r, int length, const char* origin, const char* destination, const char* airline, int curIdx);
+
+void searchRecords(RouteRecord* r, int length, const char* key1, const char* key2, SearchType st);
+
+void printMenu();
 
 #endif
