35222232524038a4a66ae9faca73301e42a2b3aa
Matthew McCabe <matthew.james.mccabe@outlook.com>
2025-04-25 10:44:44 -0500

    styling stuff

--- main.c
+++ main.c
@@ -9,18 +9,19 @@ int main( int argc, char *argv[] )
     RouteRecord *records;
     int numRecords;
     int menuChoice;
-    char key1[MAX_SIZE], key2[MAX_SIZE];
+    char key1[ MAX_SIZE ], key2[ MAX_SIZE ];
 
     
     /* 2. Check command line arguments here. If a command line argument (for the file name) is missing, print out the following: ERROR: Missing file name and end the program DONE*/
-    if (argc < 2) {
-        printf("ERROR: Missing file name\n");
+    if ( argc < 2 ) 
+    {
+        printf( "ERROR: Missing file name\n" );
         return 1;
     }
     
     /* 3. Attempt to open the file. Print out Opening <filename>... before you call fopen(). DONE*/
-    printf("Opening %s...\n", argv[1]);
-    filePtr = fopen(argv[1], "r");
+    printf( "Opening %s...\n", argv[ 1 ] );
+    filePtr = fopen( argv[1], "r" );
     
     /* 4. Check to see if the file opens. If it does not open, print out ERROR: Could not open file and end the program. DONE */
     if ( filePtr == NULL ) 
@@ -66,49 +67,51 @@ int main( int argc, char *argv[] )
     {
         printMenu( ); // 6.1 
 
-        if (scanf("%d", &menuChoice) != 1) { // 6.2
-            printf("Invalid input. Please enter a number.\n"); // 6.2
-            while (getchar() != '\n');
+        if (scanf("%d", &menuChoice) != 1) // 6.2
+        {
+            printf( "Invalid input. Please enter a number.\n" ); // 6.2
+            while ( getchar() != '\n' );
             continue;
         }
 
         // 6.3
 
         // 6.4 
-        switch (menuChoice) {
+        switch ( menuChoice ) 
+        {
             case 1: // Search by Route
-                printf("Enter origin: ");
-                scanf("%s", key1);
-                printf("Enter destination: ");
-                scanf("%s", key2);
-                searchRecords(records, numRecords, key1, key2, ROUTE);
+                printf( "Enter origin: " );
+                scanf( "%s", key1 );
+                printf( "Enter destination: " );
+                scanf( "%s", key2 );
+                searchRecords( records, numRecords, key1, key2, ROUTE );
                 break;
                 
             case 2: // Search by Origin
-                printf("Enter origin: ");
-                scanf("%s", key1);
-                searchRecords(records, numRecords, key1, NULL, ORIGIN);
+                printf( "Enter origin: " );
+                scanf( "%s", key1 );
+                searchRecords( records, numRecords, key1, NULL, ORIGIN );
                 break;
                 
             case 3: // Search by Destination
-                printf("Enter destination: ");
-                scanf("%s", key1);
-                searchRecords(records, numRecords, key1, NULL, DESTINATION);
+                printf( "Enter destination: " );
+                scanf( "%s", key1 );
+                searchRecords( records, numRecords, key1, NULL, DESTINATION );
                 break;
                 
             case 4: // Search by Airline
-                printf("Enter airline: ");
-                scanf("%s", key1);
-                searchRecords(records, numRecords, key1, NULL, AIRLINE);
+                printf( "Enter airline: " );
+                scanf( "%s", key1 );
+                searchRecords( records, numRecords, key1, NULL, AIRLINE );
                 break;
                 
             case 5: // Quit
-                printf("Good-bye\n");
-                free(records); // Free memory!!! (can't wait until i don't have to do this teehee) 6.4.2
+                printf( "Good-bye\n" );
+                free( records ); // Free memory!!! (can't wait until i don't have to do this teehee) 6.4.2
                 return 0;
                 
             default:
-                printf("Invalid option. Please try again.\n");
+                printf( "Invalid option. Please try again.\n" );
         }
     }
     
